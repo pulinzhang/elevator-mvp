dd9b28f51291b093033ea4b4a7e34412b1eea56b
zhangpulin <zhangpulin@shpubing.com>
2026-02-25 17:41:57 +0800

    Update open-next.config.ts for Cloudflare

--- open-next.config.ts
+++ open-next.config.ts
@@ -3,10 +3,26 @@ import type { OpenNextConfig } from "open-next/types/open-next";
 const config: OpenNextConfig = {
   default: {
     override: {
-      generateCacheControl: false,
+      wrapper: "cloudflare-node",
+      converter: "edge",
+      proxyExternalRequest: "fetch",
+      incrementalCache: "dummy",
+      tagCache: "dummy",
+      queue: "dummy",
+    },
+  },
+  edgeExternals: ["node:crypto"],
+  middleware: {
+    external: true,
+    override: {
+      wrapper: "cloudflare-edge",
+      converter: "edge",
+      proxyExternalRequest: "fetch",
+      incrementalCache: "dummy",
+      tagCache: "dummy",
+      queue: "dummy",
     },
   },
 };
 
 export default config;
-
